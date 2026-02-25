a61d6625fa977e9ae784ae86bac3059300ba468a
IngSeb0 <ls.contreras@uniandes.edu.co>
2026-02-25 11:19:48 -0500

    BottomBarIntegrated but not functional

--- app/src/main/java/com/example/unimarketfrontend/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/unimarketfrontend/ui/screens/HomeScreen.kt
@@ -17,6 +17,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import com.example.unimarketfrontend.viewmodel.HomeViewModel
 import com.example.unimarketfrontend.viewmodel.HomeUiState
+import com.example.unimarketfrontend.ui.components.BottomNavigationBar
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
@@ -33,11 +37,20 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
         }
 
         is HomeUiState.Success -> {
+            val currentRoute = remember{ mutableStateOf("home") }
 
             val data = state as HomeUiState.Success
+            Scaffold(bottomBar = {
 
+                BottomNavigationBar(currentRoute = currentRoute.value,
+                    onRouteChange = { newRoute ->
+                        currentRoute.value = newRoute
+                    })
+            }){innerPadding ->
             LazyColumn(
-                modifier = Modifier.fillMaxSize(),
+               modifier = Modifier
+                   .fillMaxSize()
+                   .padding(innerPadding),
                 contentPadding = PaddingValues(bottom = 80.dp)
             ) {
 
@@ -81,7 +94,9 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
                         }
                     }
                 }
+
             }
+        }
         }
     }
 }
